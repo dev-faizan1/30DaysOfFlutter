9806a3cfe115ed9c347341532ef2afec9c631b2a
Faizan004 <ch.faizan1947@gmail.com>
2022-01-30 22:14:54 +0500

    day4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:untitled/pages/home_page.dart';
 import 'package:untitled/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(
@@ -9,9 +10,10 @@ void main() {
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
+      theme: ThemeData(fontFamily: GoogleFonts.lato().fontFamily),
       routes: {
         "/": (context) => LoginPage(),
-        "/home":(context)=> HomePage(),
+        "/home": (context) => HomePage(),
       },
     ),
   );
